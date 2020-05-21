3b36bc94570e297e3d25daf235014c99608eeff3
Sibasish Mohanty <smsibasish@gmail.com>
2020-05-21 11:59:46 +0530

    intro animation added

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 const hamburgerButton = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+const introSubtitle = document.querySelector('.subtitle--intro');
 
 hamburgerButton.addEventListener('click', () => {
 	document.body.classList.toggle('nav-open');
@@ -10,3 +11,21 @@ navLinks.forEach((link) => {
 		document.body.classList.remove('nav-open');
 	});
 });
+
+const roles = JSON.parse(
+	JSON.stringify(introSubtitle.getAttribute('data-labels'))
+).split(', ');
+
+let index = 0;
+
+setInterval(() => {
+	introSubtitle.classList.remove('scale-out-hor-left');
+	introSubtitle.classList.add('scale-in-hor-left');
+	introSubtitle.innerText = roles[index];
+}, 2000);
+
+setInterval(() => {
+	introSubtitle.classList.remove('scale-in-hor-left');
+	introSubtitle.classList.add('scale-out-hor-left');
+	index = index + 1 === roles.length ? 0 : index + 1;
+}, 6000);
